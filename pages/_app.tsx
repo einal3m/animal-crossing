4e34ec5f8bf04449d11cf7d6704d8e15078ab670
Melanie Matthews <melanie.a.matthews@gmail.com>
2023-02-18 22:39:45 +1100

    Initialize nextjs app

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Card, Container } from "react-bootstrap";
+
+export default function IndexPage() {
+  return (
+    <Container>
+      <Card>
+        <Card.Body>Animal Crossing</Card.Body>
+      </Card>
+    </Container>
+  );
+}
