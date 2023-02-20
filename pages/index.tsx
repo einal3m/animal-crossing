83f0f1a50a8e8664c30df012e516aef1618b95f3
Melanie Matthews <melanie.a.matthews@gmail.com>
2023-02-20 22:05:05 +1100

    Clicking on statue image, pops up a fullscreen modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Card, Container, Table } from "react-bootstrap";
+import { Container, Table, Modal, Row, Col } from "react-bootstrap";
 import Image from "next/image";
 
 type Statue = {
@@ -64,8 +64,67 @@ export default function IndexPage() {
     },
   ];
 
+  const [showModal, setShowModal] = useState(false);
+  const [modalIndex, setModalIndex] = useState(0);
+
+  const handleStatueClick = (i: number) => {
+    setShowModal(true);
+    setModalIndex(i);
+  };
+
+  const handleClose = () => setShowModal(false);
+
   return (
     <Container>
+      <Modal show={showModal} onHide={handleClose} fullscreen>
+        <Modal.Header closeButton>
+          <Modal.Title>{`${statues[modalIndex].name} statue`} </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row className="h-75">
+            <Col xs="12" sm="6">
+              <h3>Real</h3>
+              <div
+                className="h-75"
+                style={{
+                  position: "relative",
+                }}
+              >
+                <Image
+                  style={{
+                    objectFit: "contain",
+                    objectPosition: "50% 50%",
+                  }}
+                  src={`https://einal3m.github.io/animal-crossing/images/acnh/statues/statue-${statues[modalIndex].name}-real.png`}
+                  alt={statues[modalIndex].name}
+                  fill
+                />
+              </div>
+            </Col>
+            <Col xs="12" sm="6">
+              <h3>Fake</h3>
+              {statues[modalIndex].hasFake && (
+                <div
+                  className="h-75"
+                  style={{
+                    position: "relative",
+                  }}
+                >
+                  <Image
+                    style={{
+                      objectFit: "contain",
+                      objectPosition: "50% 50%",
+                    }}
+                    src={`https://einal3m.github.io/animal-crossing/images/acnh/statues/statue-${statues[modalIndex].name}-fake.png`}
+                    alt={statues[modalIndex].name}
+                    fill
+                  />
+                </div>
+              )}{" "}
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Modal>
       <h3>Statues</h3>
       <Table bordered hover>
         <thead>
@@ -78,12 +137,13 @@ export default function IndexPage() {
           </tr>
         </thead>
         <tbody>
-          {statues.map((statue: Statue) => {
+          {statues.map((statue: Statue, i: number) => {
             return (
               <tr key={statue.name}>
                 <td>{statue.name}</td>
                 <td>
                   <Image
+                    onClick={() => handleStatueClick(i)}
                     src={`https://einal3m.github.io/animal-crossing/images/acnh/statues/statue-${statue.name}-real.png`}
                     alt={statue.name}
                     width="64"
@@ -93,6 +153,7 @@ export default function IndexPage() {
                 <td>
                   {statue.hasFake && (
                     <Image
+                      onClick={() => handleStatueClick(i)}
                       src={`https://einal3m.github.io/animal-crossing/images/acnh/statues/statue-${statue.name}-fake.png`}
                       alt={statue.name}
                       width="64"
